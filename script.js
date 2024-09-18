42750363aff9a2b743a9064b0b88860a7ca009c7
zxJustin <zxjustin@hotmail.com>
2024-09-18 15:42:40 +0800

    Add functions on maths formulas

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a , b){
+    return parseInt(a + b);
+}
+
+function subtract(a , b){
+    return parseInt(a - b);
+}
+
+function multiply(a , b){
+    return parseInt(a * b);
+}
+
+function divide(a , b){
+    return parseInt(a / b);
+}
